53ded4285009a9ff531d246f4b249775108c67b0
BrunoAlves10 <contagames00@gmail.com>
2025-09-04 23:16:09 -0300

    fix: forms corrigido

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function sendMail(){
 
     emailjs.send("service_mpge6tm","template_hycqvl4",parms).then(alert("Email Enviado!"))
 }
+
+function sendMail2(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        phone : document.getElementById("phone").value,
+        city : document.getElementById("city").value,
+        capital : document.getElementById("capital").value,
+        experience : document.getElementById("experience").value,
+        why : document.getElementById("why").value,
+    }
+
+    emailjs.send("service_mpge6tm","template_hycqvl4",parms).then(alert("Email Enviado!"))
+}
